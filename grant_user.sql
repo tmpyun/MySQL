6f89db8aa7bebbd8cb016a247725314220a6ab8e
test <123456@qq.com>
2020-12-04 09:44:28 +0800

    modifi

--- grant_user.sql
+++ grant_user.sql
@@ -53,3 +53,30 @@ mysql> show grants for xiaoming@'localhost';
 
 mysql> flush privileges;
 Query OK, 0 rows affected (0.00 sec)
+
+### <<== 主库授权用户权限
+mysql> grant select,insert,update,delete on test.* to 'xiaohong'@'10.10.10.0/255.255.255.0' identified by '123456';
+Query OK, 0 rows affected, 1 warning (0.03 sec)
+
+### <<== 从库授权用户权限
+mysql> grant select on test.* to 'xiaodong'@'10.10.10.%' identified by '123456';
+Query OK, 0 rows affected, 1 warning (0.00 sec)
+
+mysql> show grants for 'xiaohong'@'10.10.10.0/255.255.255.0';
++-------------------------------------------------------------------------------------------+
+| Grants for xiaohong@10.10.10.0/255.255.255.0                                              |
++-------------------------------------------------------------------------------------------+
+| GRANT USAGE ON *.* TO 'xiaohong'@'10.10.10.0/255.255.255.0'                               |
+| GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'xiaohong'@'10.10.10.0/255.255.255.0' |
++-------------------------------------------------------------------------------------------+
+2 rows in set (0.00 sec)
+
+mysql> show grants for 'xiaodong'@'10.10.10.%';
++-----------------------------------------------------+
+| Grants for xiaodong@10.10.10.%                      |
++-----------------------------------------------------+
+| GRANT USAGE ON *.* TO 'xiaodong'@'10.10.10.%'       |
+| GRANT SELECT ON `test`.* TO 'xiaodong'@'10.10.10.%' |
++-----------------------------------------------------+
+2 rows in set (0.00 sec)
+
